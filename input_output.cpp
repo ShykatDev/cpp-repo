5b14a5a16bd1affaeda12e7a0bac6a4bb4072ad0
Shykat Raha <devshykat2022@gmail.com>
2025-05-25 22:55:08 +0600

    input output

---
+++ input_output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x;
+    cin >> x; // Take input
+    cout << x; // Show output
+
+    char c;
+    cin >> c;
+    cout << "Character is: " << c;
+}
