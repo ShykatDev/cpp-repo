1393e558e60388e812328d9184abe9973f27b31d
Shykat Raha <devshykat2022@gmail.com>
2025-05-25 22:58:28 +0600

    EOF in cpp

---
+++ eof.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //EOF -> Doing someting while taking input
+    int x;
+
+    while(cin >> x){
+        cout << "The given input is: "<< x << endl;
+    }
+}
