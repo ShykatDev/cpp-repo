d07030e7f2a28eaf5ef0112924003ec290627cc8
Shykat Raha <devshykat2022@gmail.com>
2025-07-02 22:09:25 +0600

    insert at any position

---
+++ linked_list/insert_at_position.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert(Node *&head, int val, int pos)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+
+    for (int i = 1; i < pos; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert(head, 99, 2);
+    insert(head, 9, 1);
+
+    print(head);
+
+    return 0;
+}
