0ada495defd445524f8e59c3f96b927ab1aebf8b
Shykat Raha <devshykat2022@gmail.com>
2025-07-02 21:56:28 +0600

    linked list insert at tail

---
+++ linked_list/insert_at_tail.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL) // Corner case for empty linked list
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert_at_tail(head, 100);
+    insert_at_tail(head, 200);
+
+    print(head);
+
+    return 0;
+}
