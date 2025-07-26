a5b07c1ed416309794a5e876541ff173879de290
Shykat Raha <devshykat2022@gmail.com>
2025-07-26 21:21:27 +0600

    valid string

---
+++ Is_It_Valid.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        stack<char> st;
+
+        for (auto c : s)
+        {
+            if (st.empty())
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (c == '0' && st.top() == '1')
+                {
+                    st.pop();
+                }
+                else if (c == '1' && st.top() == '0')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push(c);
+                }
+            }
+        }
+
+        cout << (st.empty() ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
