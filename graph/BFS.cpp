b9f3d0b787cd59c939e7037fb77cdaa4ad8bef19
Shykat Raha <devshykat2022@gmail.com>
2025-08-22 20:17:51 +0600

    BFS code

---
+++ graph/BFS.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool v[1005];
+vector<int> arr[1005];
+
+void BFS(int src)
+{
+    queue<int> q;
+    q.push(src);
+
+    v[src] = true;
+
+    while (!q.empty())
+    {
+        int p = q.front();
+        q.pop();
+
+        cout << p << " ";
+
+        for (auto child : arr[p])
+        {
+            if (!v[child])
+            {
+                q.push(child);
+                v[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    arr->resize(n);
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+
+    BFS(0);
+
+    return 0;
+}
